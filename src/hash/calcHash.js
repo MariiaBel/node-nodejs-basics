aeca8d6f4c414d93e301101de5d4c7473aca5141
m.belogubova <m.belogubova@webvork.com>
2024-01-29 14:21:49 +0300

    feat: implementation hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256')
+
+    const readStream = createReadStream('./files/fileToCalculateHashFor.txt');
+    readStream.pipe(hash).setEncoding('hex').pipe(process.stdout);
 };
 
 await calculateHash();
